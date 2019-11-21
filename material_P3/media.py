3d454e550bd9c03b0bb5c0adcfac6e72abc077e2
Tu Nombre <you@example.com>
2019-11-21 13:07:58 +0100

    prueba

--- material_P3/media.py
+++ material_P3/media.py
@@ -2,27 +2,28 @@ import sys
 
 
 def avg(suma, div):
-    return (float(suma)/float(div))
+    print (float(suma)/float(div))
 
 def sum(n1, n2):
-    return (float(n1)+float(n2))
+    print (float(n1)+float(n2))
 
-def main():
-    if sys.argv[1] == None or sys.argv[2] == None or sys.argv[3] == None:
-        return -1
+def main(argv):
 
-    if "--add" in sys.argv[1]:
-        print(sum(sys.argv[2], sys.argv[3]))
-        return 0
+    # if sys.argv[1] == None or sys.argv[2] == None or sys.argv[3] == None:
+    #     return -1
 
-    elif "--avg" in sys.argv[1]:
-        print(avg(sys.argv[2], sys.argv[3]))
-        return 0
-    
-    else: 
+    if "--add" in argv:
+        sum(argv[1], argv[2])
+        exit(0)
+
+    elif "--avg" in sys.argv:
+        avg(argv[1], argv[2])
+        exit(0)
+
+    else:
         print ("ERROR")
-    
+
 
 if __name__== "__main__" :
-    main()
+    main(sys.argv[1:])
 
--- material_P3/slow_fast_time.sh
+++ material_P3/slow_fast_time.sh
@@ -3,15 +3,19 @@
 #!/bin/bash
 
 # inicializar variables
+declare -A slowTime
+declare -A fastTime
 
 # Variables de bucle interno
 Ninicio=16144
 Npaso=64
-Nfinal=17168
+Nfinal=16208
+#Nfinal=17168
 
 # Variables de bucle externo
-reps=10
-nums=(((17168-16144)/Npaso))
+reps=1
+nums=2
+#nums=$(((17168-16144)/Npaso))
 
 fDAT=slow_fast_time.dat
 fPNG=slow_fast_time.png
@@ -28,32 +32,42 @@ echo "Running slow and fast..."
 for ((i = 1 ; i <= reps ; i++)); do
 	# bucle para N desde P hasta Q
 	#for N in $(seq $Ninicio $Npaso $Nfinal);
-	for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
-		echo "N: $N / $Nfinal..."
+	#for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
+	#	echo "Slow N: $N / $Nfinal..."
 
 		# ejecutar los programas slow y fast consecutivamente con tamaño de matriz N
 		# para cada uno, filtrar la línea que contiene el tiempo y seleccionar la
 		# tercera columna (el valor del tiempo). Dejar los valores en variables
 		# para poder imprimirlos en la misma línea del fichero de datos
-		slowTime[$i,$j]=$(./slow $N | grep 'time' | awk '{print $3}')
-	done
+	#	slowTime[$i,$j]=$(./slow $N | grep 'time' | awk '{print $3}')
+	#	echo ${slowTime[$i,$j]}
+	#done
 
-	for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
+	for ((N = Ninicio, j = 1 ; N <=Nfinal ; N += Npaso, j++)); do
+		echo "Fast N: $N / $Nfinal..."
 		fastTime[$i,$j]=$(./fast $N | grep 'time' | awk '{print $3}')
 
-	done 
+	done
 done
 
 for ((j = 1 ;  j <= nums; j++)); do
-	for ((i = 1, mediaS = 0, mediaF = 0, N = Ninicio ; N <= Nfinal; i++, N += Npaso )); do
-		mediaS=$(python media.py --add mediaS slowTime[$i,$j])
-		mediaF=$(python media.py --add mediaF fastTime[$i,$j])
+	mediaF=0
+	aux=0
+#	mediaS="0"
+	for ((i = 1, N = Ninicio ; N <= Nfinal; i++, N += Npaso )); do
+		# mediaS=$(python media.py --add $mediaS {slowTime[$i,$j]})
+		echo "$aux"
+		prueba=${fastTime[$i,$j]}
+		echo "$prueba"
+		mediaF=$(python media.py --add $aux $prueba)
+		echo "$mediaF"
+		aux=$mediaF
 	done
-	finalS=$(python media.py --avg $mediaS $nums)
-	finalF=$(ptyhon media.py --avg $mediaF $nums)
-	
-	echo "$N	$finalS	$finalF" >> $fDAT
-done 
+	# finalS=$(python media.py --avg mediaS nums)
+	#finalF=$(python media.py --avg $mediaF 1)
+
+	#echo "$N	$finalS	$finalF" >> $fDAT
+done
 
 echo "Generating plot..."
 # llamar a gnuplot para generar el gráfico y pasarle directamente por la entrada
@@ -71,3 +85,4 @@ plot "$fDAT" using 1:2 with lines lw 2 title "slow", \
 replot
 quit
 END_GNUPLOT
+
