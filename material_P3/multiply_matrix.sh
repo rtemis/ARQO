6d1e756f0735ad2b5ee1d624cf9d5dc1078303d5
Tu Nombre <you@example.com>
2019-11-26 15:49:03 +0100

    Edit

--- material_P3/multiply_matrix.sh
+++ material_P3/multiply_matrix.sh
@@ -8,7 +8,7 @@ Npaso=16
 Nfinal=2048
 
 # Number of iterations
-reps=5
+reps=10
 
 # Files to be created
 fDAT=mult.dat
@@ -37,64 +37,50 @@ for ((i = 1 ; i <= reps ; i++)); do
 	# Slow loop
 	for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
 		echo "Mult Normal N: $N / $Nfinal..."
-
 		normal=$(./normal $N | grep 'time' | awk '{print $2}')
 		x=${oldN[$j]}
 		oldN[$j]=$(python -c "print( $normal + $x )")
-
-		d=$(valgrind --tool=cachegrind --I1=8192,1,64 --D1=8192,1,64 --LL=8388608,1,64 --cachegrind-out-file=temp.dat ./normal $N)
-		missesR=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $5}')
-		missesW=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $8}')
-		x=${D1mrN[$j]}
-		y=${D1mwN[$j]}
-		# Update slow value for average calculation
-		D1mrN[$j]=$(python -c "print( int('$missesR'.replace(',', '')) + $x )")
-		D1mwN[$j]=$(python -c "print( int('$missesW'.replace(',', '')) + $y )")
-
 	done
 
 	# Slow loop
 	for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
 		echo "Mult Tras N: $N / $Nfinal..."
-
 		traspuesta=$(./traspuesta $N | grep 'time' | awk '{print $2}')
 		x=${oldT[$j]}
 		oldT[$j]=$(python -c "print( $traspuesta + $x )")
-
-		d=$(valgrind --tool=cachegrind --I1=8192,1,64 --D1=8192,1,64 --LL=8388608,1,64 --cachegrind-out-file=temp.dat ./traspuesta $N)
-		missesR=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $5}')
-		missesW=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $8}')
-		x=${D1mrT[$j]}
-		y=${D1mwT[$j]}
-		# Update slow value for average calculation
-		D1mrT[$j]=$(python -c "print( int('$missesR'.replace(',', '')) + $x )")
-		D1mwT[$j]=$(python -c "print( int('$missesW'.replace(',', '')) + $y )")
-
 	done
+done
 
+for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
+	d=$(valgrind --tool=cachegrind --I1=8192,1,64 --D1=8192,1,64 --LL=8388608,1,64 --cachegrind-out-file=temp.dat ./normal $N | grep 'time' | awk '{print $2}')
+	missesR=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $5}')
+	missesW=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $8}')
+	x=${D1mrN[$j]}
+	y=${D1mwN[$j]}
+	# Update slow value for average calculation
+	D1mrN[$j]=$(python -c "print( int('$missesR'.replace(',', '')) + $x )")
+	D1mwN[$j]=$(python -c "print( int('$missesW'.replace(',', '')) + $y )")
+done
 
-
-
-
-	for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
-
-		echo "Mult Tras N: $N / $Nfinal..."
-		traspuesta=$(./multiplica_matriz $N | grep 'Traspuesta' | awk '{print $2}')
-		x=${oldT[$j]}
-		oldT[$j]=$(python -c "print( $traspuesta + $x )")
-
-	done
-
+for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
+	d=$(valgrind --tool=cachegrind --I1=8192,1,64 --D1=8192,1,64 --LL=8388608,1,64 --cachegrind-out-file=temp.dat ./traspuesta $N)
+	missesR=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $5}')
+	missesW=$(cg_annotate temp.dat | head -n 30 | grep 'PROGRAM' | awk '{print $8}')
+	x=${D1mrT[$j]}
+	y=${D1mwT[$j]}
+	# Update slow value for average calculation
+	D1mrT[$j]=$(python -c "print( int('$missesR'.replace(',', '')) + $x )")
+	D1mwT[$j]=$(python -c "print( int('$missesW'.replace(',', '')) + $y )")
 done
 
 # Loop for writing to file
 for ((N = Ninicio, j = 1 ; N <= Nfinal ; N += Npaso, j++)); do
 	timeN=$(python -c "print(${oldN[$j]} / $reps)")
 	timeT=$(python -c "print(${oldT[$j]} / $reps)")
-	normalR=$(python -c "print(${D1mrN[$j]} / $reps)")
-	normalW=$(python -c "print(${D1mwN[$j]} / $reps)")
-	trasR=$(python -c "print(${D1mrT[$j]} / $reps)")
-	trasW=$(python -c "print(${D1mwT[$j]} / $reps)")
+	normalR=$(python -c "print(${D1mrN[$j]})")
+	normalW=$(python -c "print(${D1mwN[$j]})")
+	trasR=$(python -c "print(${D1mrT[$j]})")
+	trasW=$(python -c "print(${D1mwT[$j]})")
 	echo "$N	$timeN	$normalR	$normalW	$timeT	$trasR	$trasW" >> $fDAT
 done
 
