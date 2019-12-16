685f0f0756bda06e013416568a4d216ef0d0949a
Tu Nombre <you@example.com>
2019-12-16 16:40:01 +0100

    con medias

--- P4/pi.sh
+++ P4/pi.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 N=1000
-fDAT=mult.dat
+fDAT=multpar.dat
 
 rm -f $fDAT
-
-for ((i = 1 ; i < 5 ; i++, N += 500));do
-	echo "Running $i/5"
-	p1=$(./multiply_par1 $N | grep 'time' | awk {'print $2'})
-	p2=$(./multiply_par2 $N | grep 'time' | awk {'print $2'})
-	p3=$(./multiply_par3 $N | grep 'time' | awk {'print $2'})
-	p4=$(./multiply_ser $N | grep 'time' | awk {'print $2'})
-	echo "$N  $p1  $p2  $p3  $p4" >> $fDAT
+for ((j = 1 ; j < 5 ; N += 150, j++)); do
+	echo "Running loop: $j scale $N"
+	pp1=0
+	pp2=0
+	pp3=0
+	pp4=0
+	for ((i = 1 ; i < 5 ; i++));do
+		echo "Running $i/4 of loop $j"
+		p1=$(./multiply_par1 $N | grep 'time' | awk {'print $2'})
+		pp1=$(python -c "print($pp1 + $p1)")
+		p2=$(./multiply_par2 $N | grep 'time' | awk {'print $2'})
+		pp2=$(python -c "print($pp2 + $p2)")	
+		p3=$(./multiply_par3 $N | grep 'time' | awk {'print $2'})
+		pp3=$(python -c "print($pp3 + $p3)")
+		p4=$(./multiply_ser $N | grep 'time' | awk {'print $2'})
+		pp4=$(python -c "print($pp4 + $p4)")
+	done
+	pp1=$(python -c "print($pp1 / 4)")
+	pp2=$(python -c "print($pp2 / 4)")
+	pp3=$(python -c "print($pp3 / 4)")
+	pp4=$(python -c "print($pp4 / 4)")
+	echo "Writing to file"
+	echo "$N  $pp1  $pp2  $pp3  $pp4" >> $fDAT
 done	
