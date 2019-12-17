da83fefe50b5cb978d2fcd448f1fe97d1d32088d
Tu Nombre <you@example.com>
2019-12-17 15:08:26 +0100

    p2

--- P4/pi_serie.c
+++ P4/pi_serie.c
@@ -1,19 +1,19 @@
 /*********************************************************/
-/*Cálculo PI: Método de integración - Version secuencial */
+/*Cï¿½lculo PI: Mï¿½todo de integraciï¿½n - Version secuencial */
 /*********************************************************/
 
 #include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main( int argc, char *argv[] ) 
-{ 
+int main( int argc, char *argv[] )
+{
 	int i, n = 100000000;
 	double x, h, pi, sum = 0.0, t = 0;
 	struct timeval t1,t2;
 
 	gettimeofday(&t1,NULL);
-	
+
 	h = 1.0/(double) n;
 	for (i=1;i<= n; i++)
 	{
@@ -26,6 +26,6 @@ int main( int argc, char *argv[] )
 
 	t = (t2.tv_sec -t1.tv_sec) + (t2.tv_usec - t1.tv_usec)/(double)1000000;
 	printf("Resultado pi: %f\nTiempo %lf\n", pi, t);
-		
+
 	return 0;
 }
--- P4/plot.sh
+++ P4/plot.sh
@@ -1,22 +1,24 @@
 
-fDAT=pescalar.dat
+fDAT2=aceleracion.dat
+
 
 echo "Generating plot..."
 # llamar a gnuplot para generar el gráfico y pasarle directamente por la entrada
 # estándar el script que está entre "<< END_GNUPLOT" y "END_GNUPLOT"
+
 gnuplot << END_GNUPLOT
-set title "Serie Par2 Time"
-set ylabel "Execution time (s)"
+set title "Aceleracion segun tam. vector"
+set ylabel "Aceleracion"
 set xlabel "Vector Size"
 set key right bottom
 set grid
 set term png
-set output "Serie_ParThreads_Time"
-plot "$fDAT" using 1:2 with lines lw 2 title "Serie", \
-     "$fDAT" using 1:3 with lines lw 2 title "Paralel 1 thread", \
-		 "$fDAT" using 1:4 with lines lw 2 title "Paralel 2 thread", \
-		 "$fDAT" using 1:5 with lines lw 2 title "Paralel 3 thread", \
-		 "$fDAT" using 1:5 with lines lw 2 title "Paralel 4 thread"
+set output "Aceleracion"
+plot "$fDAT2" using 1:2 with lines lw 2 title "Serie", \
+     "$fDAT2" using 1:3 with lines lw 2 title "Paralel 1 thread", \
+		 "$fDAT2" using 1:4 with lines lw 2 title "Paralel 2 thread", \
+		 "$fDAT2" using 1:5 with lines lw 2 title "Paralel 3 thread", \
+		 "$fDAT2" using 1:6 with lines lw 2 title "Paralel 4 thread"
 replot
 quit
 END_GNUPLOT
